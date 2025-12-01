224a7535e8fba9572d468bdcc217a5168a989944
Mark-Byrne-Codes <markbyrne477@gmail.com>
2025-12-01 17:19:13 +0000

    wrong folder to commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -19,9 +16,6 @@ function App() {
       </div>
       <h1 className="text-red-500">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
